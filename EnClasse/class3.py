49d369cc301e2a7f279619c6178c282845fc3ae2
Horror Face <raphaelds305@gmail.com>
2026-08-31 09:23:19 -0400

    Create class3.py

---
+++ EnClasse/class3.py
@@ -0,0 +1,15 @@
+#Raphael Desjardins
+#2026-08-31
+#Class 3
+
+print("ça marche")
+
+a = 10 # variable entiere / int
+b = 3
+c = 3.3  # variable flottante
+d = "abc" # variable char / string
+e = True # variable booléenne
+
+f = 10 + 1 #calcule
+
+print(a, b, c, d, e, f)
