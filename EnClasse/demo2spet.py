946cda19a0c10677d2153f9bd3070d45ae70bf81
Horror Face <raphaelds305@gmail.com>
2026-09-02 09:57:27 -0400

    shit

--- EnClasse/demo2spet.py
+++ EnClasse/demo2spet.py
@@ -2,4 +2,26 @@
 # 2026-09-02
 # cours 3
 
-nom = input("Quel est ton nom ?")
+import time
+
+print("Je vais deviner ton nombre")
+#peut pas mettre autre chose que un int / nombre
+chiffre = int(input("Donne moi un chiffre ? "))
+chiffre = int(chiffre)
+print(f"merci pour ce {chiffre}") 
+
+contenue_type = type(chiffre)
+
+compteur = chiffre
+compte_final = compteur + 10
+"""
+while True:
+    compteur += 1
+    print(compteur)
+    time.sleep(0.3)
+    if compteur < compte_final:
+        continue
+    break
+
+print(f"Ton nombre de base étais {chiffre} et c'est désormais {compteur}")
+"""
---
+++ EnClasse/test.py
@@ -0,0 +1,9 @@
+animal1 = "Castor"
+animal2 = "raton laveur"
+animal3 = "Renard"
+
+print("--------------")
+print(f"|{animal1:^12}|")
+print(f"|{animal2:^12}|")
+print(f"|{animal3:^12}|")
+print("--------------")
