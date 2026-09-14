e24173615ed2c0875b149306bdb6238faf12e5e7
Horror Face <raphaelds305@gmail.com>
2026-09-14 10:40:06 -0400

    Add animal profile and health scoring
    This change adds a specimen summary display, converts the animal's mass and temperature to metric values, and calculates a basic vitality score based on species, mass, and temperature ranges. It also corrects the temperature prompt text and improves the output formatting for easier review.

--- EnClasse/TP/TP1.py
+++ EnClasse/TP/TP1.py
@@ -4,6 +4,7 @@
 
 import math
 
+#Info sur l'animal
 animal = input("Entrer le nom de l'animal : ")
 
 print("requin = 1 / tigre = 2 / gnou = 3")
@@ -12,11 +13,56 @@ espece= int(input("Entrer le numéro de l'espèce : "))
 
 age = int(input("Entrez l'âge total en mois : "))
 
-temperature = float(input("Entrez la tampéature de l'animal : "))
+temperature = float(input("Entrez la tempéature de l'animal : "))
 
 masse = float(input("entrez la masse de l'animal : "))
 
 info = "INFORMATION SUR LE SPÉCIMEN"
 
+#interface
 print(f"{info:-^102}")
+print(f"Nom de l'animal : {animal}")
+print(f"Espèce de l'animal : {espece}")
+print(f"Âge de l'animal (en mois) : {age}")
+print(f"Masse de l'animal (en livres) : {masse}")
+print(f"Température corporelle de l'animal (en °F) : {temperature}")
+
+#------------------------------------------------------------------------
+#convertir mes données
+
+# livre en kilo
+masse_kg = masse * 0.45
+
+#Faren en celcsius
+temp_c = ( temperature - 32) * ( 5 / 9 )
+
+#mois en anne
+annee = age // 12
+mois = age % 12
+
+# ---------------------------------------------------------------------------
+# tableau de résultat
+score_vita = 100
+
+match espece:
+    case 1:
+        if masse_kg < 60.0 or masse_kg > 150.0 :
+            score_vita -= 20
+        if temp_c < 22.0 or temp_c > 26.0 :
+            score_vita -= 30
+    case 2:
+        if masse_kg < 60.0 or masse_kg > 150.0 :
+            score_vita -= 20
+        if temp_c < 22.0 or temp_c > 26.0 :
+            score_vita -= 30
+    case 3:
+        if masse_kg < 60.0 or masse_kg > 150.0 :
+            score_vita -= 20
+        if temp_c < 22.0 or temp_c > 26.0 :
+            score_vita -= 30
+
+
+print({score_vita})
+
+
 
